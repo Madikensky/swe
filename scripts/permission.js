9dfc0224349d093862c51c7ce2c29a75988073b3
Madiyar <madikensky2003@gmail.com>
2024-05-03 19:33:22 +0600

    feat: add data fixes

--- scripts/permission.js
+++ scripts/permission.js
@@ -3,6 +3,7 @@ btnProfile.style.color = '#fff'
 btnProfile.style.backgroundColor = '#775732'
 
 const currBody = document.querySelector('.current-body')
+const sendBtn = document.querySelector('.btn-send')
 
 const inputs = document.querySelectorAll('input[type="text"]')
 inputs[0].value = userInfo.fullName
@@ -34,15 +35,71 @@ inputs[3].addEventListener('input', (e) => {
     validityIcons[3].style.display = 'inline'
     validityIcons[2].style.display = 'none'
   } else {
-    validityIcons[3].style.display = 'none'
-    validityIcons[2].style.display = 'inline'
+    if (compareDates(inputs[2].value, inputs[3].value)) {
+      validityIcons[3].style.display = 'none'
+      validityIcons[2].style.display = 'inline'
+      sendBtn.style.pointerEvents = 'all'
+      sendBtn.removeAttribute('disabled')
+    }
   }
 })
 
+function compareDates(startDate, endDate) {
+  const startDateParts = startDate.split('/')
+  const endDateParts = endDate.split('/')
+
+  const startDateObj = new Date(
+    startDateParts[2],
+    startDateParts[1] - 1,
+    startDateParts[0]
+  )
+  const endDateObj = new Date(
+    endDateParts[2],
+    endDateParts[1] - 1,
+    endDateParts[0]
+  )
+
+  if (startDateObj < endDateObj) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const inputFile = document.getElementById('student-img')
 const submitForm = document.querySelector('.form-permission')
 const modalPerm = document.querySelector('.modal-perm')
+const inputStudent = document.querySelector(
+  '.permission-img label:nth-child(2)'
+)
 const exit = document.querySelector('.exit')
 exit.style.cursor = 'pointer'
+
+inputFile.addEventListener('input', (e) => {
+  const file = inputFile.files[0]
+  console.log(file)
+  const minSize = 10
+  const maxSize = 5000
+  const fileSize = Math.floor(file.size / 1000) //in kb
+  console.log(fileSize)
+
+  if (fileSize < minSize) {
+    alert('Small file size')
+    inputStudent.textContent = 'Choose file'
+    inputFile.value = ''
+  } else if (fileSize > maxSize) {
+    alert('Big file size')
+    inputFile.value = ''
+    inputStudent.textContent = 'Choose file'
+  } else {
+    const fileInfo = document.querySelector('.file-info')
+    fileInfo.style.display = 'flex'
+    inputStudent.textContent = file.name
+    const spans = fileInfo.querySelectorAll('p span')
+    spans[0].textContent = file.name.split('.')[file.name.split('.').length - 1]
+    spans[1].textContent = `${fileSize}kb`
+  }
+})
 
 submitForm.addEventListener('submit', (e) => {
   e.preventDefault()
